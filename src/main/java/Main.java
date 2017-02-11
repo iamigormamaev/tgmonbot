1afd4015137d315aa29725e4e9398d77f1a6bdd2
Igor Mamaev <mraction@mail.ru>
2017-02-11 15:36:08 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+/**
+ * Created by Игорь on 11.02.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+        try {
+            botsApi.registerBot(new MyMonitoringBot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
