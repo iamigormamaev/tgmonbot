10823cf669a97774956ade8e21eb0a1cf1509003
Igor Mamaev <mraction@mail.ru>
2017-09-07 22:27:31 +0300

    config changes

--- src/main/java/interfaces/Config.java
+++ src/main/java/interfaces/Config.java
@@ -3,12 +3,7 @@ package interfaces;
 public interface Config {
 
 
-/*    String BOT_NAME = "bot_name";
-    String BOT_TOKEN = "bot_token";*/
-
-
-    String BOT_NAME = "***REMOVED***";
-    String BOT_TOKEN = "***REMOVED***";
-
+    String BOT_NAME = "bot_name";
+    String BOT_TOKEN = "bot_token";
 
 }
