3d62884d6f3c9bc06e7d9c6d81868298bc96e811
Igor Mamaev <mraction@mail.ru>
2017-09-07 22:27:31 +0300

    config changes

--- src/main/java/interfaces/Config.java
+++ src/main/java/interfaces/Config.java
@@ -3,12 +3,7 @@ package interfaces;
 public interface Config {
 
 
-/*    String BOT_NAME = "bot_name";
-    String BOT_TOKEN = "bot_token";*/
-
-
-    String BOT_NAME = "tcs_mon_reminder_bot";
-    String BOT_TOKEN = "348962145:AAH5xnZaz38smpXILO6SHH-h5AlqSKLFi50";
-
+    String BOT_NAME = "bot_name";
+    String BOT_TOKEN = "bot_token";
 
 }
