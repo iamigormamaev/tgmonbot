cb216c88baee5dfa209f087082b106a8a82b6983
Igor Mamaev <mraction@mail.ru>
2017-09-13 23:33:25 +0300

    add feature "create event for me without username"

--- src/main/java/implementations/EventParserImpl.java
+++ src/main/java/implementations/EventParserImpl.java
@@ -4,11 +4,9 @@ import Exceptions.DateFromPastException;
 import Exceptions.DateParseException;
 import Exceptions.NotEnoughArgsToParseException;
 import Exceptions.NotRegisteredUserException;
-
 import interfaces.EventParser;
 import models.Event;
 import org.telegram.telegrambots.api.objects.User;
-
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -21,11 +19,9 @@ import java.util.logging.Logger;
 public class EventParserImpl implements EventParser {
     private final static Logger LOGGER = Logger.getLogger(EventParserImpl.class.getName());
     private Map<String, User> registeredUsers;
-
     public EventParserImpl(Map<String, User> registeredUsers) {
         this.registeredUsers = registeredUsers;
     }
-
     public Event parse(String inString, User author) throws NotEnoughArgsToParseException, NotRegisteredUserException, DateParseException, DateFromPastException {
         try {
             LOGGER.info("parsing string: \"" + inString + "\" author: " + author);
@@ -34,33 +30,30 @@ public class EventParserImpl implements EventParser {
             String userName = words[0];
             String dateTimeString = words[1] + " " + words[2];
             StringBuilder message = new StringBuilder("");
-            if (words.length > 3) {
-                for (int i = 3; i < words.length; i++) {
-                    message.append(words[i] + " ");
-                }
-            } else throw new ArrayIndexOutOfBoundsException();
-
             if (userName.charAt(0) == '@') {
                 userName = userName.substring(1);
             }
             if (!registeredUsers.containsKey(userName)) {
-                LOGGER.warning("NotRegisteredUserException was throw for string: \"" + inString + "\"");
-                throw new NotRegisteredUserException();
+                dateTimeString = words[0] + " " + words[1];
+                userName = author.getUserName();
+                for (int i = 2; i < words.length; i++) {
+                    message.append(words[i] + " ");
+                }
+            } else {
+                for (int i = 3; i < words.length; i++) {
+                    message.append(words[i] + " ");
+                }
             }
-
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
             LocalDateTime dateTime = LocalDateTime.parse(dateTimeString, formatter);
             Date date = Date.from(dateTime.atZone(ZoneId.systemDefault()).toInstant());
-
             if (date.before(new Date(System.currentTimeMillis()))) {
                 LOGGER.warning("DateFromPastException was throw for string: \"" + inString + "\"");
                 throw new DateFromPastException();
             }
             Event resultEvent = new Event(author, registeredUsers.get(userName), date, message.toString().trim());
-
             LOGGER.info("success parsing: \"" + inString + "\" author: " + author);
             return resultEvent;
-
         } catch (ArrayIndexOutOfBoundsException e) {
             LOGGER.warning("NotEnoughArgsToParseException was throw for string: \"" + inString + "\"");
             throw new NotEnoughArgsToParseException();
@@ -68,9 +61,5 @@ public class EventParserImpl implements EventParser {
             LOGGER.warning("DateParseException was throw for string: \"" + inString + "\"");
             throw new DateParseException();
         }
-
-
     }
-
-
 }
